8df99d45485bb1806e4371e0ec233a4131f1c5ab
ilvez <ilvez@xor.ee>
2020-12-11 09:58:02 +0200

    Add correct solution to day 11, part 1

--- day_11/1.rb
+++ day_11/1.rb
@@ -1,6 +1,10 @@
 require 'amazing_print'
 require 'pry'
 
+OCCUPIED = '#'
+FREE = 'L'
+FLOOR = '.'
+
 def columns_count
   @map.first.size
 end
@@ -11,68 +15,78 @@ end
 
 def print_map(map)
   map.each { puts(_1.join) }
+  ''
 end
 
 def occupied?(map, row, column)
-  map[row][column] == '#'
+  map[row][column] == OCCUPIED
 end
 
 def free?(map, row, column)
-  map[row][column] == 'L'
+  map[row][column] == FREE
 end
 
 def floor?(map, row, column)
-  map[row][column] == '.'
+  map[row][column] == FLOOR
 end
 
-def adjacent_occupied?(map, row, column)
+def occupied_around(map, row, column)
   (row - 1..row + 1).flat_map { |r|
-    next if r < 0 || r >= columns_count
+    next if r < 0 || r >= rows_count
     (column - 1..column + 1).map { |c|
       next if c < 0 || c >= columns_count
       next if c == column && r == row
       occupied?(map, r, c)
     }
-  }.compact.any?
+  }.compact
+end
+
+def adjacent_occupied?(map, row, column)
+  occupied_around(map, row, column).any?
 end
 
 def can_sit?(map, row, column)
   free?(map, row, column) && !adjacent_occupied?(map, row, column)
 end
 
-def sit(current_round, next_round, row, column)
-  next_round[row][column] = '#' if can_sit?(current_round, row, column)
+def must_leave?(map, row, column)
+  !floor?(map, row, column) && occupied_around(map, row, column).select { true == _1 }.size >= 4
 end
+
 
 def get_map_copy(map)
   Marshal.load(Marshal.dump(map))
 end
 
-def take_a_seat(current_round)
+def resolve_round(current_round)
   next_round = get_map_copy(current_round)
   rows_count.times.each { |row|
     columns_count.times.each { |column|
-      sit(current_round, next_round, row, column)
+      if can_sit?(current_round, row, column)
+        next_round[row][column] = OCCUPIED
+      elsif must_leave?(current_round, row, column)
+        next_round[row][column] = FREE
+      end
     }
   }
   next_round
 end
 
-@map =
-  begin
-    map = []
-    IO.readlines('test-input', chomp: true).each do
-      map << _1.chars
-    end
-    map
+def import_map(file_name)
+  map = []
+  IO.readlines(file_name, chomp: true).each do
+    map << _1.chars
   end
+  map
+end
 
+@map = import_map('input')
 current_round = @map
 
 loop do
   previous_round = get_map_copy(current_round)
-  current_round = take_a_seat(previous_round)
+  current_round = resolve_round(previous_round)
   break if current_round == previous_round
 end
 
-print_map(current_round)
+puts(current_round.flatten.select { _1 == OCCUPIED }.count)
