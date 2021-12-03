4f5fffb656c5a2589f0970eee42400fcea12d90b
ilvez <ilvez@xor.ee>
2021-12-03 17:01:22 +0200

    Add solution to 2021 day 3 part 1

--- 2021/day_03/part_1.go
+++ 2021/day_03/part_1.go
@@ -3,10 +3,36 @@ package main
 import "fmt"
 import "os"
 import "bufio"
+import "strconv"
 
 func main() {
-  inputs := FileToLines("input2")
-  counts := make([]int, len(inputs[0]))
+  inputs := FileToLines("input")
+  counts := CountBites(inputs)
+  fmt.Println("Result", CalculateGamma(counts) * CalculateEpsilon(counts))
+}
+
+func CalculateEpsilon(counts []int) int {
+  slice := ""
+  for _, value := range counts {
+    if value < 0 { slice += "1" }
+    if value >= 0 { slice += "0" }
+  }
+  result, _ := strconv.ParseInt(slice, 2, 32)
+  return int(result)
+}
+
+func CalculateGamma(counts []int) int {
+  slice := ""
+  for _, value := range counts {
+    if value < 0 { slice += "0" }
+    if value >= 0 { slice += "1" }
+  }
+  result, _ := strconv.ParseInt(slice, 2, 32)
+  return int(result)
+}
+
+func CountBites(inputs []string) (counts []int) {
+  counts = make([]int, len(inputs[0]))
   for _, inputString := range inputs {
     for i, char := range inputString {
       if char == '0' { counts[i] -= 1 }
@@ -14,21 +40,10 @@ func main() {
     }
     fmt.Println(inputString, counts)
   }
-  gammaRate := CalculateGamma(counts)
-  fmt.Println("Result", gammaRate)
+
+  return
 }
 
-func CalculateGamma(counts []int) int {
-  gammaSlice := make([]int, len(counts))
-  for i, value := range counts {
-    if value < 0 { gammaSlice[i] = '0' }
-    if value >= 0 { gammaSlice[i] = '1' }
-  }
-  fmt.Println(string(gammaSlice.Bytes())
-  return len(counts)
-}
-
-// Copypasted from internets
 func FileToLines(filePath string) (lines []string) {
   file, err := os.Open(filePath)
   if err != nil {
