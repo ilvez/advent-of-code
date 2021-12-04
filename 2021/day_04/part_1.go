8dfbe15c3cf102f159f907df0267642f5eded4d7
ilvez <ilvez@xor.ee>
2021-12-04 15:18:08 +0200

    Add solution to 2021 day 4 part 1

--- 2021/day_04/part_1.go
+++ 2021/day_04/part_1.go
@@ -7,14 +7,67 @@ import "strconv"
 import "regexp"
 
 func main() {
-  inputs := FileToLines("input2")
+  inputs := FileToLines("input")
   bingoNumbers := BingoStringToNumbers(inputs[0])
 
+  bestBoardPosition := 99999
+  winningResult := 0
+
   for i := 1; i <= NumberOfBoards(inputs); i++ {
-    fmt.Println(ParseBoard(inputs, i))
+    board := ParseBoard(inputs, i)
+    position, result := SolveBoard(board, bingoNumbers)
+    if position < bestBoardPosition {
+      bestBoardPosition = position
+      winningResult = result
+    }
   }
 
-  fmt.Println("Result", bingoNumbers)
+  fmt.Println("Result:", winningResult)
+}
+
+func SolveBoard(board [][]int, bingoNumbers []int) (winningPosition int, solution int) {
+  markedColumns := make([]int, 5)
+  markedRows := make([]int, 5)
+  for position, bingoNumber := range bingoNumbers {
+    rowPos, colPos := NumberPositionOnBoard(board, bingoNumber)
+    if rowPos == -1 || colPos == -1 { continue }
+    markedRows[rowPos] += 1
+    markedColumns[colPos] += 1
+    board[rowPos][colPos] = -1
+    if (IsBingo(markedRows) || IsBingo(markedColumns)) {
+      return position, FindSolution(board, bingoNumber)
+    }
+  }
+  fmt.Println("-------", markedColumns, markedRows)
+  return -1, -1
+}
+
+func FindSolution(board [][]int, winningNumber int) int {
+  solution := 0
+  for _, row := range board {
+    for _, number := range row {
+      if number == -1 { continue }
+      solution += number
+    }
+  }
+
+  return solution * winningNumber
+}
+
+func IsBingo(markedPositions []int) bool {
+  for _, posCount := range markedPositions { if posCount == 5 { return true} }
+  return false
+}
+
+func NumberPositionOnBoard(board [][]int, number int) (rowPos int, colPos int) {
+  for rowPos, row := range board {
+    for colPos, boardNumber := range row {
+      if boardNumber == number {
+        return rowPos, colPos
+      }
+    }
+  }
+  return -1, -1
 }
 
 func ParseBoard(inputs []string, boardNumber int) [][]int {
