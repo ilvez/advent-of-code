f32856b34dfa389936d9d17a5c27634f56d72516
ilvez <ilvez@xor.ee>
2021-12-05 09:21:33 +0200

    Add position parsing

--- 2021/day_05/part_1.go
+++ 2021/day_05/part_1.go
@@ -2,7 +2,26 @@ package main
 
 import "fmt"
 import aoc "aocgo/aochelper"
+import "strings"
 
 func main() {
-  fmt.Println("Result:", aoc.FileToLines("input2"))
+  inputs := aoc.FileToLines("input2")
+  for _, input := range inputs {
+    start, end := ParseLine(input)
+    fmt.Println("Result:", start, end)
+  }
+}
+
+func ParseLine(line string) (Pos, Pos) {
+  unparsedPositions := strings.Split(line, " -> ")
+  return ParsePos(unparsedPositions[0]), ParsePos(unparsedPositions[1])
+}
+
+func ParsePos(unparsedPosition string) Pos {
+  posStrings := strings.Split(unparsedPosition, ",")
+  return Pos { aoc.StringToInt(posStrings[0]), aoc.StringToInt(posStrings[1])}
+}
+
+type Pos struct {
+  x, y int
 }
