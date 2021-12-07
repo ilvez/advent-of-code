8e94f519442224cc85ec258e9b6e0c9bb9a996ee
ilvez <ilvez@xor.ee>
2021-12-07 09:48:34 +0200

    Add 2021/07 part 1 solution

--- 2021/day_07/main.go
+++ 2021/day_07/main.go
@@ -1,11 +1,48 @@
 package main
 
 import "fmt"
+import "math"
 import aoc "aocgo/aochelper"
 
 
 func main() {
-  inputString := aoc.FileToLines("input2")
-  inputArray := aoc.StringToIntArray(inputString[0])
-  fmt.Println(inputArray)
+  inputString := aoc.FileToLines("input")
+  crabLocations := aoc.StringToInt64Array(inputString[0])
+  fmt.Println("Part 1 result:", FindCheapestFuel(crabLocations, FindFuelPart1))
 }
+
+func FindCheapestFuel(crabLocations []int64, fuelFunc func([]int64, int64) int64) int64 {
+  var cheapestFuel int64 = math.MaxInt64 - 1
+  minLoc, maxLoc := findMinAndMax(crabLocations)
+  for i := minLoc; i <= maxLoc; i++ {
+    fuel := fuelFunc(crabLocations, i)
+    // fmt.Println(i, fuel)
+    if fuel < cheapestFuel {
+      cheapestFuel = fuel
+    }
+  }
+  return cheapestFuel
+}
+
+func FindFuelPart1(crabLocations []int64, goToLocation int64) (fuelAmount int64) {
+  fuelAmount = 0
+  for _, location := range crabLocations {
+    fuelAmount += aoc.AbsInt64(goToLocation - location)
+  }
+  return
+}
+
+func findMinAndMax(a []int64) (min, max int64) {
+  min = a[0]
+  max = a[0]
+  for _, value := range a {
+    if value < min {
+      min = value
+    }
+    if value > max {
+      max = value
+    }
+  }
+  return
+}
+
--- go/aochelper/AocHelper.go
+++ go/aochelper/AocHelper.go
@@ -9,6 +9,12 @@ func Abs(x int) int {
   if x < 0 { return -x }
   return x
 }
+
+func AbsInt64(x int64) int64 {
+  if x < 0 { return -x }
+  return x
+}
+
 func StringToInt(input string) int {
   a, _ := strconv.Atoi(input)
   return a
@@ -19,6 +25,16 @@ func StringToIntArray(line string) []int {
   re := regexp.MustCompile(`\d+`)
   for _, numberString := range re.FindAllString(line, -1) {
     outputs = append(outputs, StringToInt(numberString))
+  }
+  return outputs
+}
+
+func StringToInt64Array(line string) []int64 {
+  outputs := make([]int64, 0)
+  re := regexp.MustCompile(`\d+`)
+  for _, numberString := range re.FindAllString(line, -1) {
+    number, _ := strconv.ParseInt(numberString, 10, 64)
+    outputs = append(outputs, number)
   }
   return outputs
 }
