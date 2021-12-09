be1a9d76c1eb53e357eba934e332044346250d99
ilvez <ilvez@xor.ee>
2021-12-09 12:53:17 +0200

    Add part 1 to day 9

--- 2021/day_09/main.go
+++ 2021/day_09/main.go
@@ -6,12 +6,111 @@ import(
   _ "strings"
 )
 
+type direction int
 
-func main() {
-  parseMap("input2")
+const (
+  up direction = 1
+  down direction = 0
+)
+
+type Point struct {
+  value int
+  n, s, e, w direction
 }
 
-func parseMap(fileName string) {
-  inputLines := aoc.FileToLines(fileName)
-  fmt.Println(inputLines)
+type Map [][]Point
+
+func main() {
+  depthMap := parseMap("input")
+  parseDirections(&depthMap)
+  printPart1Result(&depthMap)
+}
+
+
+func printPart1Result(depthMap *Map) {
+  riskLevel := 0
+  for _, row := range(*depthMap) {
+    for _, point := range(row) {
+      if point.n == up && point.e == up && point.s == up && point.w == up {
+        riskLevel += 1 + point.value
+      }
+    }
+  }
+  fmt.Println("Part 1 result:", riskLevel)
+}
+
+func parseDirections(depthMap *Map) {
+  rows := len(*depthMap)
+  for y := 0; y < rows; y++ {
+    cols := len((*depthMap)[y])
+    for x := 0; x < cols; x++ {
+      currentPoint := (*depthMap)[y][x]
+      if y - 1 >= 0 {
+        if currentPoint.value < (*depthMap)[y-1][x].value {
+          (*depthMap)[y][x].n = up
+        }
+      } else {
+        (*depthMap)[y][x].n = up
+      }
+      if x + 1 < cols {
+        if currentPoint.value < (*depthMap)[y][x+1].value {
+          (*depthMap)[y][x].e = up
+        }
+      } else {
+        (*depthMap)[y][x].e = up
+      }
+      if y + 1 < rows {
+        if currentPoint.value < (*depthMap)[y+1][x].value {
+          (*depthMap)[y][x].s = up
+        }
+      } else {
+        (*depthMap)[y][x].s = up
+      }
+      if x - 1 >= 0 {
+        if currentPoint.value < (*depthMap)[y][x-1].value {
+          (*depthMap)[y][x].w = up
+        }
+      } else {
+        (*depthMap)[y][x].w = up
+      }
+    }
+  }
+}
+
+func parseMap(fileName string) (depthMap Map) {
+  lines := aoc.FileToLines(fileName)
+  depthMap = make([][]Point, len(lines))
+  for i, line := range lines {
+    depthMap[i] = make([]Point, len(line))
+    for j, pointString := range line {
+      depthMap[i][j] = Point {
+        value: aoc.StringToInt(string(pointString)),
+      }
+    }
+  }
+  return
+}
+
+func printPoint(point Point) {
+  fmt.Print(
+    fmt.Sprint(
+      "[",
+      point.value,
+      ", nesw:",
+      point.n,
+      point.e,
+      point.s,
+      point.w,
+      "]",
+    ),
+  )
+}
+
+func printMap(depthMap *Map) {
+  for _, row := range(*(depthMap)) {
+    for _, point := range(row) {
+      printPoint(point)
+    }
+    fmt.Println()
+  }
 }
