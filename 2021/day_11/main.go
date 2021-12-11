fda8a07621ba3da8a8963596ba8efb3cee1dabc6
Taavi Ilves <ilvez@xor.ee>
2021-12-11 14:18:24 +0200

    Add base for day 11

---
+++ 2021/day_11/main.go
@@ -0,0 +1,21 @@
+package main
+
+import(
+  aoc "aocgo/aochelper"
+  "fmt"
+  _"os"
+)
+
+func main() {
+  inputFile := "input"
+  printPart1Solution(inputFile)
+  printPart2Solution(inputFile)
+}
+
+func printPart1Solution(inputFile string) {
+  fmt.Println("Part 1 solution:", inputFile)
+}
+
+func printPart2Solution(inputFile string) {
+  fmt.Println("Part 2 solution:", inputFile)
+}
