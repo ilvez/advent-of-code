fd8ccf4a651727e6b08537c1db3f23dc807d96e2
Taavi Ilves <ilvez@xor.ee>
2021-12-12 11:46:18 +0200

    Add node type parsing

--- 2021/day_12/main.go
+++ 2021/day_12/main.go
@@ -10,15 +10,29 @@ func main() {
   fmt.Println(parseEdges(inputFile))
 }
 
+func cleanDeadEdges(edges *[]Edge) {
+}
+
 func parseEdges(inputFile string) (edges []Edge) {
   lines := aoc.FileToLines(inputFile)
   for _, line := range lines {
     nodes := strings.Split(line, "-")
-    edges = append(edges, Edge { a: nodes[0], b: nodes[1] })
+    a := Node { value: nodes[0], isBig: isBig(nodes[0])}
+    b := Node { value: nodes[1], isBig: isBig(nodes[1])}
+    edges = append(edges, Edge { a: a, b: b })
   }
   return
 }
 
+type Node struct {
+  value string
+  isBig bool
+}
+
 type Edge struct {
-  a, b string
+  a, b Node
+}
+
+func isBig(s string) bool {
+  return strings.ToUpper(s) == s
 }
