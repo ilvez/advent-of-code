5039cb04a5951cdf85b317bb358467775d81c6ef
Taavi Ilves <ilvez@xor.ee>
2021-12-12 11:39:54 +0200

    Add input parsing to edge array

--- 2021/day_12/main.go
+++ 2021/day_12/main.go
@@ -3,11 +3,20 @@ package main
 import(
   aoc "aocgo/aochelper"
   "fmt"
+  "strings"
 )
 func main() {
   inputFile := "input2"
+  fmt.Println(parseEdges(inputFile))
+}
+
+func parseEdges(inputFile string) (edges []Edge) {
   lines := aoc.FileToLines(inputFile)
-  fmt.Println(lines)
+  for _, line := range lines {
+    nodes := strings.Split(line, "-")
+    edges = append(edges, Edge { a: nodes[0], b: nodes[1] })
+  }
+  return
 }
 
 type Edge struct {
