4d35ab53eacc34dd0fd8cc4009f2a0bfc4cf878f
Taavi Ilves <ilvez@xor.ee>
2022-12-01 08:39:42 +0200

    Add solution to day 1

---
+++ 2022/src/01/solution.ts
@@ -0,0 +1,36 @@
+import * as fs from 'fs'
+import * as path from 'path'
+const _ = require('lodash')
+
+function fileToStringArray(fileName: string):string[] {
+  const projectDir = process.env.PWD || '.'
+  const filePath = path.join(projectDir, fileName)
+  return fs.readFileSync(filePath, 'utf-8').split(/\n/)
+}
+
+function getNumberArray(content:string[]):number[] {
+  return content.map(function(item:string) {
+    return +item
+  })
+}
+
+function getCarryAmounts(content:number[]):number[] {
+  var result:number[] = []
+  var current:number = 0
+  content.forEach(
+    function(item:number){
+      if (item == 0) {
+        result.push(current)
+        current = 0
+      } else {
+        current += item
+      }
+    }
+  )
+  return result
+}
+
+const amounts:number[] = getCarryAmounts(getNumberArray(fileToStringArray('src/01/input')))
+
+console.log(_.max(amounts))
+console.log(_.sum(amounts.sort((a: number, b:number) => b - a).slice(0, 3)))
