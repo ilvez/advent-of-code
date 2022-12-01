76425856cd759546189dbd7f4817ae3394ce8363
Taavi Ilves <ilvez@xor.ee>
2022-12-01 22:06:59 +0200

    Use normal sort

--- 2022/src/01/solution.js
+++ 2022/src/01/solution.js
@@ -1,11 +1,9 @@
-import { SortService, Direction } from "https://deno.land/x/sort@v1.1.1/mod.ts"
-
 const numArray = Deno
   .readTextFileSync(Deno.args[0])
   .split(/\n\n/)
   .map((text) => text.split(/\n/))
   .map((array) => array.map((i) => +i).reduce((sum, current) => sum + current, 0))
 
-const amounts = SortService.sort(numArray, Direction.DESCENDING)
+const amounts = numArray.sort((a, b) => b - a)
 console.log(`Day 01 part 1: ${amounts[0]}`)
 console.log(`Day 01 part 2: ${amounts.slice(0, 3).reduce((sum, current) => sum + current, 0)}`)
