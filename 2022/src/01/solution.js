34a25b47da3144fa1b2bc2734842b056b2bc6f69
Taavi Ilves <ilvez@xor.ee>
2022-12-01 21:29:49 +0200

    fixup! Add deno based solution

--- 2022/src/01/solution.js
+++ 2022/src/01/solution.js
@@ -1,22 +1,8 @@
-import * as fs from 'fs'
-import * as path from 'path'
-import * as _ from 'lodash'
+import { SortService, Direction } from "https://deno.land/x/sort@v1.1.1/mod.ts"
 
-function fileToStringArray(fileName) {
-  const projectDir = process.env.PWD || '.'
-  const filePath = path.join(projectDir, fileName)
-  return fs.readFileSync(filePath, 'utf-8').split(/\n/)
-}
-
-function getNumberArray(content){
-  return content.map(function(item) {
-    return +item
-  })
-}
-
-function getCarryAmounts(content) {
-  result = []
-  current = 0
+const getCarryAmounts = (content) => {
+  const result = []
+  let current = 0
   content.forEach((item) => {
     if (item == 0) {
       result.push(current)
@@ -25,11 +11,13 @@ function getCarryAmounts(content) {
       current += item
     }
   })
-
   return result
 }
+const numArray = Deno
+  .readTextFileSync(Deno.args[0])
+  .split(/\n/)
+  .map((i) => +i);
 
-const amounts = getCarryAmounts(getNumberArray(fileToStringArray('src/01/input')))
-
-console.log(_.max(amounts))
-console.log(_.sum(amounts.sort((a, b) => b - a).slice(0, 3)))
+const amounts = SortService.sort(getCarryAmounts(numArray), Direction.DESCENDING)
+console.log(amounts[0])
+console.log(amounts.slice(0, 3).reduce((sum, current) => sum + current, 0))
