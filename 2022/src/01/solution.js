84166cab85c4a6146a9e285fc27f18c2bedcbe8e
Taavi Ilves <ilvez@xor.ee>
2022-12-01 22:00:08 +0200

    More conscise solution

--- 2022/src/01/solution.js
+++ 2022/src/01/solution.js
@@ -1,23 +1,11 @@
 import { SortService, Direction } from "https://deno.land/x/sort@v1.1.1/mod.ts"
 
-const getCarryAmounts = (content) => {
-  const result = []
-  let current = 0
-  content.forEach((item) => {
-    if (item == 0) {
-      result.push(current)
-      current = 0
-    } else {
-      current += item
-    }
-  })
-  return result
-}
 const numArray = Deno
   .readTextFileSync(Deno.args[0])
-  .split(/\n/)
-  .map((i) => +i);
+  .split(/\n\n/)
+  .map((text) => text.split(/\n/))
+  .map((array) => array.map((i) => +i).reduce((sum, current) => sum + current, 0))
 
-const amounts = SortService.sort(getCarryAmounts(numArray), Direction.DESCENDING)
-console.log(amounts[0])
-console.log(amounts.slice(0, 3).reduce((sum, current) => sum + current, 0))
+const amounts = SortService.sort(numArray, Direction.DESCENDING)
+console.log(`Day 01 part 1: ${amounts[0]}`)
+console.log(`Day 01 part 2: ${amounts.slice(0, 3).reduce((sum, current) => sum + current, 0)}`)
