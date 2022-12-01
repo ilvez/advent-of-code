2519a3bdbb97dcd3fc77c12cae01569992bb19c2
Taavi Ilves <ilvez@xor.ee>
2022-12-01 22:09:01 +0200

    Simple logging

--- 2022/src/01/solution.js
+++ 2022/src/01/solution.js
@@ -5,5 +5,5 @@ const numArray = Deno
   .map((array) => array.map((i) => +i).reduce((sum, current) => sum + current, 0))
 
 const amounts = numArray.sort((a, b) => b - a)
-console.log(`Day 01 part 1: ${amounts[0]}`)
-console.log(`Day 01 part 2: ${amounts.slice(0, 3).reduce((sum, current) => sum + current, 0)}`)
+console.log('Day 01 part 1:', amounts[0])
+console.log('Day 01 part 2:', amounts.slice(0, 3).reduce((sum, current) => sum + current, 0))
