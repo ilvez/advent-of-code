4d9cc56a798d5011f5103f3bdf4a248cd0965d2f
Taavi Ilves <ilvez@xor.ee>
2022-12-01 22:11:57 +0200

    Less brackets

--- 2022/src/01/solution.js
+++ 2022/src/01/solution.js
@@ -1,8 +1,8 @@
 const numArray = Deno
   .readTextFileSync(Deno.args[0])
   .split(/\n\n/)
-  .map((text) => text.split(/\n/))
-  .map((array) => array.map((i) => +i).reduce((sum, current) => sum + current, 0))
+  .map(text => text.split(/\n/))
+  .map(array => array.map(i => +i).reduce((sum, current) => sum + current, 0))
 
 const amounts = numArray.sort((a, b) => b - a)
 console.log('Day 01 part 1:', amounts[0])
