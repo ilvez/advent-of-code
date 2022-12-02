a428d0717f2bd50b18e894382561622f53823730
Taavi Ilves <ilvez@xor.ee>
2022-12-02 15:43:25 +0200

    Optimize code

--- 2022/src/02/solution.js
+++ 2022/src/02/solution.js
@@ -1,75 +1,38 @@
-const abcToChoice = (a) => {
-  switch (a) {
-    case 'A':
-      return 'rock';
-    case 'B':
-      return 'paper';
-    case 'C':
-      return 'scissors';
-  }
-};
-
-const choicePoints = (a) => {
-  switch (a) {
-    case 'rock':
-      return 1;
-    case 'paper':
-      return 2;
-    case 'scissors':
-      return 3;
-  }
-};
-
-const roundToScore = (they, we) => {
-  if (they == we) return choicePoints(we) + 3;
-  else if (
-    (they == 'rock' && we == 'paper') ||
-    (they == 'paper' && we == 'scissors') ||
-    (they == 'scissors' && we == 'rock')
-  ) return choicePoints(we) + 6;
-  else return choicePoints(we);
-};
-
-const part1XYZToABC = (a) => {
-  switch (a) {
-    case 'X': // rock
-      return 'A';
-    case 'Y': // paper
-      return 'B';
-    case 'Z': // scissors
-      return 'C';
-  }
+const decideOnABC = (they, we) => {
+  const points = +(we.replace(/A/, 1).replace(/B/, 2).replace(/C/, 3));
+  if (they == we) return points + 3;
+  if (add(we) == they) return points;
+  return points + 6;
 };
 
 const part1DecideResult = (pair) => {
-  const they = abcToChoice(pair[0]);
-  const we = abcToChoice(part1XYZToABC(pair[1]));
+  const we = pair[1].replace(/X/, 'A').replace(/Y/, 'B').replace(/Z/, 'C');
 
-  return roundToScore(they, we);
+  return decideOnABC(pair[0], we);
 };
 
-const part2XYZToChoice = (they, xyz) => {
-  switch (xyz) {
-    case 'X': //lose
-      if (they == 'rock') return 'scissors';
-      if (they == 'paper') return 'rock';
-      if (they == 'scissors') return 'paper';
-      break;
-    case 'Y': // draw
+const add = (
+  choice,
+) => (String.fromCharCode(choice.charCodeAt(0) + 1 > 67 ? 65 : choice.charCodeAt(0) + 1));
+const sub = (
+  choice,
+) => (String.fromCharCode(choice.charCodeAt(0) - 1 < 65 ? 67 : choice.charCodeAt(0) - 1));
+
+const part2XYZToChoice = (they, we) => {
+  switch (we) {
+    case 'X':
+      return sub(they);
+    case 'Y':
       return they;
-    case 'Z': // win
-      if (they == 'rock') return 'paper';
-      if (they == 'paper') return 'scissors';
-      if (they == 'scissors') return 'rock';
-      break;
+    case 'Z':
+      return add(they);
   }
 };
 
 const part2DecideResult = (pair) => {
-  const they = abcToChoice(pair[0]);
-  const we = part2XYZToChoice(they, pair[1]);
+  const we = part2XYZToChoice(pair[0], pair[1]);
 
-  return roundToScore(they, we);
+  return decideOnABC(pair[0], we);
 };
 
 const parsedInput = Deno
@@ -78,11 +41,7 @@ const parsedInput = Deno
   .filter((pair) => pair != '')
   .map((text) => text.split(/ /));
 
-console.log(
-  'Day 02 part 1:',
-  parsedInput.map(part1DecideResult).reduce((sum, current) => sum + current, 0),
-);
-console.log(
-  'Day 02 part 2:',
-  parsedInput.map(part2DecideResult).reduce((sum, current) => sum + current, 0),
-);
+const part1 = parsedInput.map(part1DecideResult).reduce((sum, current) => sum + current, 0);
+const part2 = parsedInput.map(part2DecideResult).reduce((sum, current) => sum + current, 0);
+console.log('Day 02 part 1:', part1, '-> correct:', part1 == 11906);
+console.log('Day 02 part 2:', part2, '-> correct:', part2 == 11186);
