72a5d36bfe039af3defbe6349e65ce760f138c3a
Taavi Ilves <ilvez@xor.ee>
2022-12-03 11:37:34 +0200

    Make more readable

--- 2022/src/03/solution.js
+++ 2022/src/03/solution.js
@@ -1,7 +1,6 @@
 import { ld } from 'https://x.nest.land/deno-lodash@1.0.0/mod.ts';
 
 const priority = (ch) => {
-  if (ch == '') return 0;
   const asciiValue = ch.charCodeAt(0);
   if (asciiValue >= 97) return asciiValue - 96;
   else return asciiValue - 64 + 26;
@@ -9,20 +8,22 @@ const priority = (ch) => {
 
 const findCommon = (...packs) => {
   const common = ld.flatten(ld.intersection(...packs.map((p) => p.split(''))));
-  if (common.length == 0) return 0;
-  return common[0];
+  return common.length == 1 ? common[0] : 0;
 };
 
-const groupByN = (n, data) => {
+const groupByThree = (input) => {
   const result = [];
-  for (let i = 0; i < data.length; i += n) result.push(data.slice(i, i + n));
+  for (let i = 0; i < input.length; i += 3) result.push(input.slice(i, i + 3));
   return result;
 };
 
-const input = Deno
-  .readTextFileSync(Deno.args[0])
-  .split(/\n/)
-  .filter((a) => a.length > 1);
+const groupIntoTwoPacks = (input) =>
+  input.map(
+    (content) => [
+      content.slice(0, content.length / 2),
+      content.slice(content.length / 2, content.length),
+    ],
+  );
 
 const solution = (group) =>
   group.map((pack) => priority(findCommon(...pack))).reduce(
@@ -30,12 +31,10 @@ const solution = (group) =>
     0,
   );
 
-const part1Input = input.map(
-  (content) => [
-    content.slice(0, content.length / 2),
-    content.slice(content.length / 2, content.length),
-  ],
-);
+const input = Deno
+  .readTextFileSync(Deno.args[0])
+  .split(/\n/)
+  .filter((a) => a.length > 1);
 
-console.log('Day 03 part 1:', solution(part1Input));
-console.log('Day 03 part 2:', solution(groupByN(3, input)));
+console.log('Day 03 part 1:', solution(groupIntoTwoPacks(input)));
+console.log('Day 03 part 2:', solution(groupByThree(input)));
