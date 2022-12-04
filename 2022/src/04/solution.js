f991f863037613fa91bd10c351edcfd94c9f90cf
Taavi Ilves <ilvez@xor.ee>
2022-12-04 12:11:50 +0200

    Fix linting

--- 2022/src/04/solution.js
+++ 2022/src/04/solution.js
@@ -6,26 +6,26 @@ const input = Deno
   .filter((a) => a.length > 1);
 
 const fullRange = (line) => {
-    const assignments = line.split(',')
-    const a = assignments[0].split('-')
-    const b = assignments[1].split('-')
-    return [ld.range(+a[0], +a[1] + 1), ld.range(+b[0], +b[1] + 1)]
-}
+  const assignments = line.split(',');
+  const a = assignments[0].split('-');
+  const b = assignments[1].split('-');
+  return [ld.range(+a[0], +a[1] + 1), ld.range(+b[0], +b[1] + 1)];
+};
 
 const fullyContains = (input) => {
   return input.filter((line) => {
-    const range = fullRange(line)
-    const intersection = ld.intersection(range[0], range[1])
-    return intersection.length == range[0].length || intersection.length == range[1].length
-  }).length
-}
+    const range = fullRange(line);
+    const intersection = ld.intersection(range[0], range[1]);
+    return intersection.length == range[0].length || intersection.length == range[1].length;
+  }).length;
+};
 
 const partiallyContains = (input) => {
   return input.filter((line) => {
-    const range = fullRange(line)
-    return ld.intersection(range[0], range[1]).length > 0
-  }).length
-}
+    const range = fullRange(line);
+    return ld.intersection(range[0], range[1]).length > 0;
+  }).length;
+};
 
 console.log('Day 03 part 1:', fullyContains(input));
 console.log('Day 03 part 1:', partiallyContains(input));
