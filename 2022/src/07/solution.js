cc1a321807c8773596c65a5994eae00aca31cea4
Taavi Ilves <ilvez@xor.ee>
2022-12-07 20:13:37 +0200

    Add day 7 part 1 working solution

--- 2022/src/07/solution.js
+++ 2022/src/07/solution.js
@@ -1,51 +1,51 @@
+import { ld } from 'https://x.nest.land/deno-lodash@1.0.0/mod.ts';
+
 const input = Deno
   .readTextFileSync(Deno.args[0])
-  .split(/\n/)
+  .split(/\n/);
 
-window.lineNumber = -1
+window.lineNumber = -1;
 
 const parseTree = (input) => {
-  return treeTraverse(input, 0)
-}
+  return treeTraverse(input, 0);
+};
 
 const treeTraverse = (input) => {
-  lineNumber += 1
-  const currentTree = {size: 0}
+  lineNumber += 1;
+  const currentTree = { size: 0 };
   for (; lineNumber < input.length; lineNumber += 1) {
-    const line = input[lineNumber]
-    const command = line.split(' ')
-    if (command[1] == "cd") {
+    const line = input[lineNumber];
+    const command = line.split(' ');
+    if (command[1] == 'cd') {
       if (command[2] == '..') {
-        return currentTree
+        return currentTree;
       } else {
-        currentTree[command[2]] = treeTraverse(input)
-        currentTree.size += currentTree[command[2]].size
+        currentTree[command[2]] = treeTraverse(input);
+        currentTree.size += currentTree[command[2]].size;
       }
     } else if (+command[0] > 0) {
-      currentTree.size += +command[0]
+      currentTree.size += +command[0];
     }
   }
-  return currentTree
-}
+  return currentTree;
+};
 
-const directories = (directory) => {
-  return Object
-    .keys(directory).filter(entry => entry != 'size')
-    .reduce((obj, subDirectory) => {
-      return { ...obj, [subDirectory]: directory[subDirectory].size }
-    }, {})
-}
-
-const flatten = (tree) => {
-  const dirs = directories(tree)
-  const subDirectories = Object.keys(dirs).flatMap((subDir) => flatten(tree[subDir]))
-  console.log(subDirectories)
-  return {
-    ...dirs,
-    ...subDirectories[0]
+function flattenObject(object, prefix = '', result = {}) {
+  if (ld.isNumber(object)) {
+    result[prefix] = object;
+  } else {
+    for (const element in object) flattenObject(object[element], prefix + '/' + element, result);
   }
+  return result;
 }
 
-const fileTree = parseTree(input)
+const fileTree = parseTree(input);
 
-console.log(`Day 7 part 1:`, Object.entries(flatten(fileTree)).filter(entry => entry[1] < 100000).map(entry => entry[1]).reduce((sum, current) => sum + current))
+const flattenedArray = Object.entries(flattenObject(fileTree));
+console.log(
+  `Day 7 part 1:`,
+  flattenedArray.filter((entry) => entry[1] < 100000).map((entry) => entry[1]).reduce((
+    sum,
+    current,
+  ) => sum + current),
+);
