4dfa4ccee5335b354cc13c0f9bba456f9966e0fc
Taavi Ilves <taavi.ilves@every-pay.com>
2024-11-30 16:07:27 +0200

    2023 day 1

--- 2023/aoc/lib/day01.ex
+++ 2023/aoc/lib/day01.ex
@@ -1,12 +1,32 @@
 defmodule Day01 do
+  def run do
+    IO.puts("Day 01, part 1: #{part1("test/resources/day01/input")}")
+    IO.puts("Day 01, part 2: #{part2("test/resources/day01/input")}")
+  end
+
   def part1(file) do
     lines(file)
-    |> numbers()
+    |> numbers1()
     |> Enum.sum()
   end
 
-  def numbers(lines) do
+  def part2(file) do
+    lines(file)
+    |> numbers2()
+    |> Enum.sum()
+  end
+
+  def numbers1(lines) do
     lines
+    |> Stream.map(&String.replace(&1, ~r/[^\d]/, ""))
+    |> Stream.map(&reduce_string/1)
+    |> Stream.map(&String.to_integer/1)
+    |> Enum.to_list()
+  end
+
+  def numbers2(lines) do
+    lines
+    |> Stream.map(&word_replace/1)
     |> Stream.map(&String.replace(&1, ~r/[^\d]/, ""))
     |> Stream.map(&reduce_string/1)
     |> Stream.map(&String.to_integer/1)
@@ -16,12 +36,35 @@ defmodule Day01 do
   def lines(file) do
     File.stream!(file)
       |> Stream.map(&String.trim/1)
-      |> Stream.map(&String.split(&1, ","))
       |> Enum.to_list()
       |> List.flatten()
+  end
+
+  defp reduce_string(string) when byte_size(string) <1 do
+    "00"
   end
 
   defp reduce_string(string) do
     String.first(string) <> String.last(string)
   end
+
+  def word_replace(string) do
+    parse_both(string)
+    |> Enum.map(&Integer.to_string/1)
+    |> Enum.join()
+  end
+
+  def parse_both(""), do: []
+  def parse_both(<<c>>   <> rest) when c in ?0..?9, do: [c - ?0 | parse_both(rest)]
+  def parse_both("zero"  <> rest), do: [0 | parse_both("o" <> rest)]
+  def parse_both("one"   <> rest), do: [1 | parse_both("e" <> rest)]
+  def parse_both("two"   <> rest), do: [2 | parse_both("o" <> rest)]
+  def parse_both("three" <> rest), do: [3 | parse_both("e" <> rest)]
+  def parse_both("four"  <> rest), do: [4 | parse_both("r" <> rest)]
+  def parse_both("five"  <> rest), do: [5 | parse_both("e" <> rest)]
+  def parse_both("six"   <> rest), do: [6 | parse_both("x" <> rest)]
+  def parse_both("seven" <> rest), do: [7 | parse_both("n" <> rest)]
+  def parse_both("eight" <> rest), do: [8 | parse_both("t" <> rest)]
+  def parse_both("nine"  <> rest), do: [9 | parse_both("e" <> rest)]
+  def parse_both(<<_>>   <> rest), do: parse_both(rest)
 end
--- 2023/aoc/test/day01_test.exs
+++ 2023/aoc/test/day01_test.exs
@@ -2,12 +2,37 @@ defmodule Day01Test do
   use ExUnit.Case
   doctest Day01
 
-  test "gives lines" do
+  test "part 1" do
     test_input = "test/resources/day01/test_input"
     lines = Day01.lines(test_input)
     assert lines == ["1abc2", "pqr3stu8vwx", "a1b2c3d4e5f", "treb7uchet"]
-    assert Day01.numbers(lines) == [12, 38, 15, 77]
-    assert Day01.part1(test_input) == 142
-    IO.puts("Day01 part1: #{Day01.part1("test/resources/day01/input")}")
+    assert Day01.numbers1(lines) == [12, 38, 15, 77]
+    part1 = Day01.part1(test_input)
+    assert part1 == 142
+  end
+
+  test "part 2" do
+    test_input = "test/resources/day01/test_input2"
+    lines = Day01.lines(test_input)
+    assert lines == [
+      "two1nine",
+      "eightwothree",
+      "abcone2threexyz",
+      "xtwone3four",
+      "4nineeightseven2",
+      "zoneight234",
+      "7pqrstsixteen"
+    ]
+
+    assert Day01.word_replace("two1nine") == "219"
+    assert Day01.word_replace("eightwothree") == "823"
+    assert Day01.word_replace("abcone2threexyz") == "123"
+    assert Day01.word_replace("eightnineseventwo1seven") == "897217"
+    assert Day01.word_replace("9h1xcrcggtwo38") == "91238"
+    assert Day01.word_replace("bnjqlftwobvsvjqptdp1two94twonej") == "2129421"
+
+    # IO.puts(Day01.numbers2(lines))
+    assert Day01.numbers2(lines) == [29, 83, 13, 24, 42, 14, 76]
+    assert Day01.part2(test_input) == 281
   end
 end
