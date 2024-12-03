753d649aac525ac8b2fd6e29924bf13515d9aa67
Taavi Ilves <taavi.ilves@every-pay.com>
2024-12-03 15:04:22 +0200

    2024 day 3, part 2

--- 2024/aoc/lib/day03.ex
+++ 2024/aoc/lib/day03.ex
@@ -1,14 +1,23 @@
 defmodule Day03 do
   def run do
     IO.puts("Day 01, part 1: #{part1("test/resources/day03/input")}")
-    # IO.puts("Day 01, part 2: #{part2("test/resources/day03/input")}")
+    IO.puts("Day 01, part 2: #{part2("test/resources/day03/input")}")
   end
 
   def part1(file) do
     file
     |> memory()
+    |> arguments()
     |> multiply()
   end
+
+  def part2(file) do
+    file
+    |> memory()
+    |> selected_arguments()
+    |> multiply()
+  end
+
 
   def memory(file) do
     File.stream!(file)
@@ -16,8 +25,25 @@ defmodule Day03 do
     |> Enum.join("")
   end
 
-  def multiply(memory) do
+  def arguments(memory) do
     Regex.scan(~r"mul\((\d+),(\d+)\)", memory, capture: :all_but_first)
+  end
+
+  def selected_arguments(memory) do
+    {:ok, memory} = Regex.scan(~r"mul\((\d+),(\d+)\)|don't\(\)|do\(\)", memory)
+    |> Enum.reduce(%{on: true, memory: []}, fn
+      ["don't()"], acc -> %{on: false, memory: acc.memory}
+      ["do()"], acc -> %{on: true, memory: acc.memory}
+      [_, a, b], %{on: true} = acc -> %{on: true, memory: acc.memory ++ [[a, b]]}
+      _, acc -> acc
+    end)
+    |> Map.fetch(:memory)
+
+    memory
+  end
+
+  def multiply(arguments) do
+    arguments
     |> Enum.map(fn [a, b] -> String.to_integer(a) * String.to_integer(b) end)
     |> Enum.sum()
   end
--- 2024/aoc/test/day03_test.exs
+++ 2024/aoc/test/day03_test.exs
@@ -6,11 +6,11 @@ defmodule Day03Test do
     test_file = "test/resources/day03/test_input"
     memory = Day03.memory(test_file)
     assert memory == "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-    assert Day03.multiply(memory) == 161
+    assert Day03.multiply(Day03.arguments(memory)) == 161
   end
 
   test "part 2" do
-    # test_file = "test/resources/day03/test_input"
-    # assert Day03.part2(test_file) == 4
+    memory = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+    assert Day03.selected_arguments(memory) == [["2", "4"], ["8", "5"]]
   end
 end
