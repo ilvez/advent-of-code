023e189b21b30bb1aa24fc572623a2116d8858ea
Taavi Ilves <taavi.ilves@every-pay.com>
2024-12-03 10:49:03 +0200

    2024 day 3, part 1

--- 2024/aoc/lib/day03.ex
+++ 2024/aoc/lib/day03.ex
@@ -1,7 +1,13 @@
 defmodule Day03 do
   def run do
-    # IO.puts("Day 01, part 1: #{part1("test/resources/day03/input")}")
+    IO.puts("Day 01, part 1: #{part1("test/resources/day03/input")}")
     # IO.puts("Day 01, part 2: #{part2("test/resources/day03/input")}")
+  end
+
+  def part1(file) do
+    file
+    |> memory()
+    |> multiply()
   end
 
   def memory(file) do
@@ -10,7 +16,9 @@ defmodule Day03 do
     |> Enum.join("")
   end
 
-  def multiplications(memory) do
-    Regex.scan(~r"mul(.*)", memory)
+  def multiply(memory) do
+    Regex.scan(~r"mul\((\d+),(\d+)\)", memory, capture: :all_but_first)
+    |> Enum.map(fn [a, b] -> String.to_integer(a) * String.to_integer(b) end)
+    |> Enum.sum()
   end
 end
--- 2024/aoc/test/day03_test.exs
+++ 2024/aoc/test/day03_test.exs
@@ -6,7 +6,7 @@ defmodule Day03Test do
     test_file = "test/resources/day03/test_input"
     memory = Day03.memory(test_file)
     assert memory == "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-    assert Day03.multiplications(memory) == ""
+    assert Day03.multiply(memory) == 161
   end
 
   test "part 2" do
