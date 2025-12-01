5f6c6940cdef17bb81e9dd7d1739a3c1d16844d0
Taavi Ilves <ilvez@xor.ee>
2025-12-01 10:33:29 +0200

    feat: 2025 day 01 part 2

--- 2025/01/main.py
+++ 2025/01/main.py
@@ -1,20 +1,22 @@
-file = open("test_input2", "r")
 file = open("input", "r")
 
 position = 50
 count = 0
 for line in file.readlines():
     line = line.strip()
-    direction, amount = line[0], int(line[1:][-2:])
-    if direction == "L":
-        position = position - amount
-    else:
-        position += amount
+    amount = int(line[1:])
+
+    prev_pos = position
+    count += amount // 100
+    position = position + amount % 100 if line[0] != "L" else position - amount % 100
 
     if position < 0:
         position = 100 + position
+        if prev_pos > 0:
+            count += 1
     elif position > 100:
         position = position - 100
+        count += 1
     elif position == 100:
         position = 0
     if position == 0:
