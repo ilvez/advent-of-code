2c955df305b60942c630d0360c8fa21db9df1ea9
Taavi Ilves <ilvez@xor.ee>
2025-12-04 10:09:29 +0200

    2025 day 04 part 2

--- 2025/04/main.py
+++ 2025/04/main.py
@@ -1,7 +1,4 @@
-path = 'test_input'
-path = 'input'
-
-floor_plan = [[x for x in y.strip()] for y in open(path).readlines()]
+from copy import deepcopy
 
 def safe_coord(coord, x, y):
     x1, y1 = coord[0], coord[1]
@@ -12,14 +9,59 @@ def safe_coords(x, y):
     return [c for c in [(i, j) for i in [x - 1, x, x + 1] for j in [y - 1, y, y + 1]] if safe_coord(c, x, y)]
 
 
-def neighbour_count(coords):
-    return len([paper for paper in [floor_plan[y][x] for (x, y) in coords] if paper == '@'])
+def neighbour_count(coords, floors):
+    return len([paper for paper in [floors[y][x] for (x, y) in coords] if paper == '@'])
 
 
-result = 0
-for y in range(0, len(floor_plan)):
-    for x in range(0, len(floor_plan[y])):
-        if floor_plan[y][x] == '@':
-            if neighbour_count(safe_coords(x, y)) < 4:
+def papers_accessible(floor_plan):
+    result = 0
+    for y in range(0, len(floor_plan)):
+        for x in range(0, len(floor_plan[y])):
+            if floor_plan[y][x] == '@':
+                if neighbour_count(safe_coords(x, y), floor_plan) < 4:
+                    result += 1
+    return result
+
+
+def count_papers(floor_plan):
+    result = 0
+    for y in range(0, len(floor_plan)):
+        for x in range(0, len(floor_plan[y])):
+            if floor_plan[y][x] == '@':
                 result += 1
-print(f"Part 1: {result}")
+    return result
+
+
+def print_map(floors):
+    for i in floors:
+        print(''.join(map(str, i)))
+
+
+def modify_floor_plan(floors):
+    new_floor = deepcopy(floors)
+    for y in range(0, len(floors)):
+        for x in range(0, len(floors[y])):
+            if floors[y][x] == '@':
+                if neighbour_count(safe_coords(x, y), floors) < 4:
+                    new_floor[y][x] = '.'
+    return new_floor
+
+
+path = 'test_input'
+path = 'input'
+
+floor_plan = [[x for x in y.strip()] for y in open(path).readlines()]
+
+print(f"Part 1: {papers_accessible(floor_plan)}")
+
+map_copy = deepcopy(floor_plan)
+count = -1
+
+while True:
+    prev_count = count
+    map_copy = modify_floor_plan(map_copy)
+    count = count_papers(map_copy)
+    if count == prev_count:
+        break
+
+print(f"Part 2: {count_papers(floor_plan) - count_papers(map_copy)}")
